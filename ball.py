6fc08c66ed689670ddf69a4d2e4b1e81cccd9252
drc <double-oh-seven@web.de>
2016-12-29 19:12:21 +0100

    Clean up code

--- ball.py
+++ ball.py
@@ -15,7 +15,7 @@ class Ball:
   # in order to match the logic for the bar, size is added to the ball twice
   size = 1
 
-  pixels = [(x,y) for x in range(size * -1, size + 1) for y in range(size * -1, size + 1)]
+  pixels = [(x, y) for x in range(size * -1, size + 1) for y in range(size * -1, size + 1)]
 
   def __init__(self):
     angle = float(random.randrange(-30, 30))
@@ -23,14 +23,11 @@ class Ball:
       angle += 180
     self.speedy = math.sin(angle / 180 * math.pi)
     self.speedx = math.cos(angle / 180 * math.pi)
-    print("angle: {} ".format(angle))
-    print("y: {} ".format(self.speedy))
-    print("x: {} ".format(self.speedx))
 
   def updatePos(self):
-    self.posx += self.speedx*self.speed
-    self.posy += self.speedy*self.speed
+    self.posx += self.speedx * self.speed
+    self.posy += self.speedy * self.speed
 
   def getPixels(self):
-    return [(int(self.posx+x),int(self.posy+y)) for x,y in self.pixels]
+    return [(int(self.posx + x), int(self.posy + y)) for x,y in self.pixels]
 
